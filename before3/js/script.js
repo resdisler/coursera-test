184079566863844ea027a89099badcd821b88296
resdisler <res.disler@gmx.ch>
2018-11-19 20:43:47 +0100

    categories 5

--- before3/js/script.js
+++ before3/js/script.js
@@ -24,7 +24,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
   var dc = {};
 
   var homeHtml = "snippets/home-snippet.html";
-  var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
+  var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
   var categoriesTitleHtml = "snippets/categories-title-snippet.html";
   var categoriesHtml = "snippets/category-snippet.html";
 
